f4a6ab86069bdbca10e54e4cf8a14cb761920e36
Quilmont <repo@quilmont.com>
2017-07-11 11:19:31 -0400

    ParkRight pages have been added with their interfaces

--- passport-automation/src/test/java/driver/deviceAndroid.java
+++ passport-automation/src/test/java/driver/deviceAndroid.java
@@ -18,18 +18,16 @@ public class deviceAndroid {
 	public static final String appPackage = "com.passportparking.mobile";
 	*/
 
-	//JK 2
-	public static final String appiumVersion = "1.6.4";
+	//JK 
 	public static final String platformName = "Android";
-	public static final String deviceName = "NotUsed";
+	public static final String deviceName = "Emulator3";
 	public static final String device = "Android";
-	public static final String automationName = "UiAutomator2";
 	public static final String URL = "http://127.0.0.1:4723/wd/hub";
 	public static final String browserNameChrome = "Chrome";
 	public static final String browserNameFirefox = "Firefox";
 	
-	public static final String appName = "mobilepay-release.apk";
-	public static final String appDir = "/Users/Anand/Testing/Office/passport-automation/apps";
+	public static final String appName = "ParkRight-staging.apk";
+	public static final String appDir = "/Users/jameskoch/Downloads";
 	public static final String appPackage = "com.passportparking.mobile";
 	
 }
