ec9dede8aa152b3fa11b9cbe665757a5a6a283db
Anand <anand.ravi@passportinc.com>
2017-07-24 20:47:41 +0530

    updated few dependencies

--- passport-automation/src/test/java/Pages/BasePage.java
+++ passport-automation/src/test/java/Pages/BasePage.java
@@ -1,25 +1,32 @@
 package Pages;
 
-
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 
-import org.openqa.selenium.By;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
-import java.util.List;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-public abstract class BasePage {
+import driver.deviceAndroid;
+import driver.deviceIOS;
 
+public abstract class BasePage {
+	
 	@FindBy (id = "menuButton")
 	public MobileElement menuIcon;
 	
@@ -62,17 +69,27 @@ public abstract class BasePage {
 	@FindBy (id = "tutorialStepMessage")
 	public  MobileElement loginMenuOption;
 	
-		
-	static WebDriverWait wait;
-	
     private static final int KEYBOARD_ANIMATION_DELAY = 1000;
     private static final int XML_REFRESH_DELAY = 1000;
+    
+    /*public static final String excelPath = "/Users/Anand/Testing/Office/passport-automation/";
+	public static final String excelName = "Automation Sheet.xlsx";
+	public static final String excelSheetName = "TestData";*/
 
-    protected final AppiumDriver driver;
+    protected static AppiumDriver driver;
+    
+	private static XSSFSheet ExcelWSheet;
+	private static XSSFWorkbook ExcelWBook;
+	private static XSSFCell Cell;
+    private static XSSFRow Row;
 
     public BasePage(AppiumDriver driver) {
     	this.driver = driver;
         PageFactory.initElements(new AppiumFieldDecorator(driver, 35, TimeUnit.SECONDS), this);
+    }
+    
+    public static void excelFileReading() throws Exception {
+    	/*ExcelReading.setExcelFile(excelPath + excelName, excelSheetName);*/
     }
     
     protected boolean sendKeysToElement(String input, WebElement element, boolean appendNewLine) throws InterruptedException {
@@ -101,13 +118,16 @@ public abstract class BasePage {
 		return screenshot.renameTo(new File(screenshotDirectory, String.format("%s.png", name)));
 	}
 	
-	
-	
 	public static void sleep(int sleeptime) {
 		try {
 			Thread.sleep(sleeptime);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static void waitForClickability(MobileElement mobileElementLocator) {
+		WebDriverWait wait = new WebDriverWait(driver, 20);
+		wait.until(ExpectedConditions.elementToBeClickable(mobileElementLocator));
 	}
 }
