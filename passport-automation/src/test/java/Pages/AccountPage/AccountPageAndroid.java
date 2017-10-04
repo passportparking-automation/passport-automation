187acfb1daf838c84788c5c0e9282f4a7e393912
joeakel <joeakel@qms-macbook-pro.sc.rr.com>
2017-10-04 13:26:41 -0400

    JA - More FindBys added to the following pages.

--- passport-automation/src/test/java/Pages/AccountPage/AccountPageAndroid.java
+++ passport-automation/src/test/java/Pages/AccountPage/AccountPageAndroid.java
@@ -1,9 +1,32 @@
 package Pages.AccountPage;
 
+import org.openqa.selenium.support.FindBy;
+
 import Pages.BasePage;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
 
 public class AccountPageAndroid extends BasePage implements AccountPage {
+	
+	@FindBy(id="accountName")
+	public MobileElement accountName;
+	
+	@FindBy(id="accountEmail")
+	public MobileElement accountEmail;
+	
+	@FindBy(id="accountPhone")
+	public MobileElement accountPhone;
+	
+	/*
+	 * These are the ID names for the icons and titles contained inside the buttons.
+	 *
+	@FindBy(id="menuItemTitle")
+	public MobileElement menuItemTitle;
+	
+	@FindBy(id="menuItemIcon")
+	public MobileElement menuItemIcon;
+	*/
+	
 
 	public AccountPageAndroid(AppiumDriver driver) {super(driver);}
 
--- passport-automation/src/test/java/Pages/PaymentMethodPage/PaymentMethodPageAndroid.java
+++ passport-automation/src/test/java/Pages/PaymentMethodPage/PaymentMethodPageAndroid.java
@@ -1,9 +1,24 @@
 package Pages.PaymentMethodPage;
 
+import org.openqa.selenium.support.FindBy;
+
 import Pages.BasePage;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
 
 public class PaymentMethodPageAndroid extends BasePage implements PaymentMethodPage{
+	
+	@FindBy(id="payment_method_type_image")
+	public MobileElement paymentMethodTypeImage;
+	
+	@FindBy(id="card_nickname")
+	public MobileElement cardNickname;
+	
+	@FindBy(id="cardholder_name")
+	public MobileElement cardholderName;
+	
+	@FindBy(id="change_card_button")
+	public MobileElement changeCardButton;
 
 	public PaymentMethodPageAndroid(AppiumDriver driver) {super(driver);}
 
